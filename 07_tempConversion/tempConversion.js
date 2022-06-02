edb0e67ef27160b89c9c1dc9072ed798e870f931
Kate Wolf <whoiskateidkher@gmail.com>
2022-06-02 08:56:27 -0400

    Complete javascript exercise 7 - tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(initialTemp) {
+  return Math.round((initialTemp - 32) * (5/9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(initialTemp) {
+  return Math.round((initialTemp * (9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
