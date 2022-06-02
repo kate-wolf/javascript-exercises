70d3318de007064698f89fbd5877a55cc7de46f1
Kate Wolf <whoiskateidkher@gmail.com>
2022-06-02 06:34:06 -0400

    Complete javascript exercises 1-4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...values) {
+  for (const value of values) {
+    let element = 0;
+    do {
+      if (array[element] === value) {
+        array.splice(element, 1);
+      }
+      element++;
+    } while (element < array.length)
+  } 
+  return array;
 };
+
+// removeFromArray([1, 2, 3, 4], 1, 2, 3, 4)
 
 // Do not edit below this line
 module.exports = removeFromArray;
