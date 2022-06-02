4bc06df485389876c5347260fbe78e31430e719d
Kate Wolf <whoiskateidkher@gmail.com>
2022-06-02 07:55:15 -0400

    Complete javascript exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+  if (((firstNumber || secondNumber) < 0) || (!Number.isFinite(firstNumber)) || (!Number.isFinite(secondNumber))) {
+    return 'ERROR';
+  }
+  let biggerNumber = 0;
+  if (firstNumber >= secondNumber) {
+    biggerNumber = firstNumber; 
+  }
+  else {
+    biggerNumber = secondNumber;
+  }
+  let result = biggerNumber;
+  for (let i = 1; i < biggerNumber; i++) {
+    result += biggerNumber - i;
+  }
+  return result;
 };
 
 // Do not edit below this line
